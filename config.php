193346b0757a4b332ad6a78068c5827c85cf4ca0
Chirag Dayaramani <68194613+chiragdayaramani@users.noreply.github.com>
2021-07-17 16:19:08 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+define('DB_SERVER','localhost');
+define('DB_USERNAME','id17216719_root');
+define('DB_PASSWORD','<D{0vbh0jPb>>dP=');
+define('DB_NAME','id17216719_bbank');
+
+//try connecting to the database
+
+$con=mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
+
+
+?>
